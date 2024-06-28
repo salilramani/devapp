7009dcec25ca4ebd9ca1651ef4494dd619eb798d
salilramani <salilramani@gmail.com>
2024-06-28 14:46:30 -0400

    ongoings

--- app/components/Markupcard.jsx
+++ app/components/Markupcard.jsx
@@ -6,7 +6,7 @@ const Markupcard = ({ id, user_name, user_email, markup_url }) => {
     <Link href={`/Markups/${id}`}>
       <div className="max-w-sm rounded overflow-hidden shadow-lg transition transform hover:scale-105 hover:shadow-2xl cursor-pointer bg-white m-2">
         <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2 text-gray-900">{user_name}'s Markup</div>
+          <div className="font-bold text-xl mb-2 text-gray-900">{user_name} Markup</div>
           <p className="text-gray-600 text-base mb-2">{user_email}</p>
           <p className="text-gray-600 text-base mb-2">{markup_url}</p>
           <p className="text-blue-500 text-base">Click here to open the markup file.</p>
