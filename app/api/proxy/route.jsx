e1c59f473c6a43b99e9bef3dd46ebfb93193856f
salilramani <salilramani@gmail.com>
2024-06-29 11:09:38 -0400

    proxy testing

--- app/api/proxy/route.jsx
+++ app/api/proxy/route.jsx
@@ -4,22 +4,26 @@ import httpProxy from 'http-proxy';
 // Create a proxy server
 const proxy = httpProxy.createProxyServer({ changeOrigin: true });
 
-export async function GET(request) {
-  const { searchParams } = new URL(request.url);
+export async function GET(req) {
+  const { searchParams } = new URL(req.url);
   const targetUrl = searchParams.get('url');
 
   if (!targetUrl) {
+    console.error('URL is required');
     return NextResponse.json({ error: 'URL is required' }, { status: 400 });
   }
 
+  console.log(`Proxying request to: ${targetUrl}`);
+
   return new Promise((resolve, reject) => {
-    proxy.web(request, request.res, { target: targetUrl }, (error) => {
+    proxy.web(req, req.res, { target: targetUrl }, (error) => {
       if (error) {
+        console.error('Proxy error:', error);
         reject(NextResponse.json({ error: 'Proxy error', details: error.message }, { status: 500 }));
       }
       resolve();
     });
   });
-};
+}
 
 export const dynamic = 'force-dynamic';
